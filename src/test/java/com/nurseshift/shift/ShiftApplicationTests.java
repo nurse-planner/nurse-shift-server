a2d14e298d97024784465b525fea931451488ce2
penguin_03 <penguin_03@naver.com>
2023-09-27 14:17:49 +0900

    init: project setting

---
+++ src/test/java/com/nurseshift/shift/ShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurseshift.shift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
