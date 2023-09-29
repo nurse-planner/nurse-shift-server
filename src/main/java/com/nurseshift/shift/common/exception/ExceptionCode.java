b145a9ee88546e03d1558c9a803503d9d1e5fea9
zqwz <zqwz@naver.com>
2023-09-29 18:16:12 +0900

    feat: 예외 코드 추가
    간호사 정보를 찾지 못하는 경우의 예외 코드 추가

--- src/main/java/com/nurseshift/shift/common/exception/ExceptionCode.java
+++ src/main/java/com/nurseshift/shift/common/exception/ExceptionCode.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ExceptionCode {
 
+    /* 404 CONFLICT : 데이터를 찾을 수 없음 */
+    NURSE_NOT_FOUND(HttpStatus.NOT_FOUND, "Nurse Not Found"),
+
     /* 409 CONFLICT : 중복된 데이터가 존재 */
     ID_DUPLICATE(HttpStatus.CONFLICT, "Id Is Duplicate");
 
