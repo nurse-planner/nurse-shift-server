68e8ae1ed2720fe7c099021561e451a8ab23577c
zqwz <zqwz@naver.com>
2023-10-03 17:06:21 +0900

    feat: 로그인 response data 추가

--- src/main/java/com/nurseshift/shift/member/MemberDto.java
+++ src/main/java/com/nurseshift/shift/member/MemberDto.java
@@ -16,10 +16,23 @@ public class MemberDto {
     @Getter
     public static class Response {
 
+        private final Long id;
         private final String email;
 
         public Response(Member member) {
+            this.id = member.getId();
             this.email = member.getEmail();
+        }
+    }
+
+    @Getter
+    public static class JwtResponse {
+        private final String jwt;
+        private final Response user;
+
+        public JwtResponse(String jwt, Member member) {
+            this.jwt = jwt;
+            this.user = new Response(member);
         }
     }
 }
