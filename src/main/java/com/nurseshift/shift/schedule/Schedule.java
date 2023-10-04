a346e7fa9238fa3c7fbc745207dab35d3f280cf6
zqwz <zqwz@naver.com>
2023-10-05 00:38:51 +0900

    feat: 근무표 entity 추가

--- src/main/java/com/nurseshift/shift/schedule/Schedule.java
+++ src/main/java/com/nurseshift/shift/schedule/Schedule.java
@@ -1,9 +1,8 @@
 package com.nurseshift.shift.schedule;
 
+import com.nurseshift.shift.member.Member;
 import com.nurseshift.shift.nurse.Nurse;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -16,18 +15,35 @@ public class Schedule {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "schedule_id")
     private Long id;
+    private Long idx;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
     @Column(name = "date", nullable = false)
     private LocalDate date;
     @Column(name = "shift_type", nullable = false)
-    private Integer shiftType;
+    private String shiftType;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "nurse_id", nullable = false)
     private Nurse nurse;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 
-    public Schedule(Long id, LocalDate date, Integer shiftType) {
-        this.id = id;
-        this.date = date;
-        this.shiftType = shiftType;
+    public void setNurse(Nurse nurse) {
+        this.nurse = nurse;
+        if (!nurse.getSchedules().contains(this)) {
+            nurse.getSchedules().add(this);
+        }
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+        if (!member.getSchedules().contains(this)) {
+            member.getSchedules().add(this);
+        }
     }
 }
